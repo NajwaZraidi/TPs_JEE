b690cb82db0697da552ed56fea3966105d6410e5
NajwaZraidi <najwazraidi7@gmail.com>
2023-03-13 09:47:52 +0100

    Modification devoir 4

---
+++ Devoir4/src/main/java/ma/enset/devoir4/Devoir4Application.java
@@ -0,0 +1,13 @@
+package ma.enset.devoir4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devoir4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devoir4Application.class, args);
+    }
+
+}
---
+++ Devoir4/src/test/java/ma/enset/devoir4/Devoir4ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.devoir4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devoir4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

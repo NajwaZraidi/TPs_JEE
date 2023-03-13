3b3a1672f5616e51e6511978593458ade95d2cdb
NajwaZraidi <najwazraidi7@gmail.com>
2023-03-13 10:05:15 +0100

    Modification devoir 4

--- Devoir4/src/main/java/ma/enset/Devoir4Application.java
+++ Devoir4/src/main/java/ma/enset/Devoir4Application.java
@@ -1,4 +1,4 @@
-package ma.enset.devoir4;
+package ma.enset;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Devoir4/src/test/java/ma/enset/Devoir4ApplicationTests.java
+++ Devoir4/src/test/java/ma/enset/Devoir4ApplicationTests.java
@@ -1,4 +1,4 @@
-package ma.enset.devoir4;
+package ma.enset;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

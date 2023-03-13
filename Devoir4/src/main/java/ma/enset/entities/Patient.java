70120397e65b5b896c7976d0b7ea62cd9660304a
NajwaZraidi <najwazraidi7@gmail.com>
2023-03-13 10:07:40 +0100

    Modification devoir 4

---
+++ Devoir4/src/main/java/ma/enset/entities/Patient.java
@@ -0,0 +1,12 @@
+package ma.enset.entities;
+
+import java.util.Date;
+
+public class Patient {
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private boolean malade;
+
+
+}
